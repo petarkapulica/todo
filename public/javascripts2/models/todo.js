596d173524bbf48a5c979e934e9c6cc441c766a6
petarkapulica <epkapuleti@gmail.com>
2015-08-22 18:57:29 +0200

    get model refactored

--- public/javascripts2/models/todo.js
+++ public/javascripts2/models/todo.js
@@ -2,12 +2,8 @@ var Todo = Todo || {};
 //kontroler komunicira samo s ovim
 Todo.TodoModel = function(){
 
-  var getFromLocalStorage = function(criteria){
-    return Todo.TodoModelGet().getFromLocalStorage(criteria);
-  };
-
   return {
-    get : getFromLocalStorage,
+    get : Todo.TodoModelGet().getFromLocalStorage,
     countLeftItems : Todo.TodoModelCount().count,
     insert : Todo.TodoModelInsert().insert,
     currentTodo : Todo.TodoModelGet().getCurrentTodo,
--- public/javascripts2/models/todo/get.js
+++ public/javascripts2/models/todo/get.js
@@ -2,109 +2,34 @@ var Todo = Todo || {};
 
 Todo.TodoModelGet = function(){
 
-  var todos = [];
-
   var getFromLocalStorage = function(criteria){
     var myTodos = localStorage.todos !== undefined ?
     JSON.parse(localStorage.todos) : [];
+    var todos = [];
+    if(myTodos)
+    {
+      $.each(myTodos,function(key, value){
+        if(checkStatus(criteria, value))
+        {
+          todos.push(value);
+        }
+      });
+    }
+    return todos;
+  };
+
+  var checkStatus = function(criteria, value){
     switch(criteria) {
-        case 'all': return getAllTodos(myTodos);
-        break;
-        case 'active': return getActiveTodos(myTodos);
-        break;
-        case 'completed': return getCompletedTodos(myTodos);
-        break;
-        case 'deleted': return getDeletedTodos(myTodos);
-        break;
-        default: return getTodos(myTodos);
+      case 'all': return !value.deleted;
+      break;
+      case 'active': return !value.completed && !value.deleted;
+      break;
+      case 'completed': return value.completed && !value.deleted;
+      break;
+      case 'deleted': return value.deleted;
+      break;
+      default: return true;
     };
-  };
-
-  var getAllTodos = function(myTodos){
-    todos = [];
-    if(myTodos)
-    {
-      $.each(myTodos,function(key,value){
-        if(!value.deleted)
-        {
-          todos.push(value);
-        }
-      });
-      return todos;
-    }
-    else
-    {
-      return false;
-    }
-  };
-
-  var getActiveTodos = function(myTodos){
-    todos = [];
-    if(myTodos)
-    {
-      $.each(myTodos,function(key, value){
-        if(!value.completed && !value.deleted)
-        {
-            todos.push(value);
-        }
-      });
-      return todos;
-    }
-    else
-    {
-      return false;
-    }
-  };
-
-  var getCompletedTodos = function(myTodos){
-    todos = [];
-    if(myTodos)
-    {
-      $.each(myTodos,function(key, value){
-        if(value.completed && !value.deleted)
-        {
-          todos.push(value);
-        }
-      });
-      return todos;
-    }
-    else
-    {
-      return false;
-    }
-  };
-
-  var getDeletedTodos = function(myTodos){
-    todos = [];
-    if(myTodos)
-    {
-      $.each(myTodos, function(key, value){
-        if(value.deleted)
-        {
-          todos.push(value);
-        }
-      });
-      return todos;
-    }
-    else
-    {
-      return false;
-    }
-  };
-
-  var getTodos = function(myTodos){
-    todos = [];
-    if(myTodos)
-    {
-      $.each(myTodos,function(key, value){
-        todos.push(value);
-      });
-      return todos;
-    }
-    else
-    {
-      return false;
-    }
   };
 
   var getCurrentTodo = function(){
@@ -113,11 +38,11 @@ Todo.TodoModelGet = function(){
     var highest = Number.NEGATIVE_INFINITY;
     var tmp;
     for (var i = todoArr.length - 1 ; i >= 0 ; i--) {
-        tmp = todoArr[i].modified;
-        if (tmp > highest)
-        {
-            return todoArr[i].order;
-        }
+      tmp = todoArr[i].modified;
+      if (tmp > highest)
+      {
+          return todoArr[i].order;
+      }
     }
   };
 
