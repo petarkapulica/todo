6f92db6aeab59a5e24792f4003b6f4122659c8ec
petarkapulica <epkapuleti@gmail.com>
2015-08-22 18:23:57 +0200

    all works

--- public/javascripts2/models/todo.js
+++ public/javascripts2/models/todo.js
@@ -2,26 +2,13 @@ var Todo = Todo || {};
 //kontroler komunicira samo s ovim
 Todo.TodoModel = function(){
 
-  var remaining = 0;
-
   var getFromLocalStorage = function(criteria){
     return Todo.TodoModelGet().getFromLocalStorage(criteria);
   };
 
-  var countLeftItems = function(){
-    remaining = 0;
-    $.each( getFromLocalStorage(),function(key,value){
-      if(!value.completed && !value.deleted)
-      {
-        remaining ++;
-      }
-    });
-    return remaining;
-  };
-
   return {
     get : getFromLocalStorage,
-    countLeftItems : countLeftItems,
+    countLeftItems : Todo.TodoModelCount().count,
     insert : Todo.TodoModelInsert().insert,
     currentTodo : Todo.TodoModelGet().getCurrentTodo,
     update : Todo.TodoModelUpdate().update,
--- public/javascripts2/models/todo/count.js
+++ public/javascripts2/models/todo/count.js
@@ -2,4 +2,25 @@ var Todo = Todo || {};
 
 Todo.TodoModelCount = function(){
 
-}
+  var remaining = 0;
+
+  var getFromLocalStorage = function(criteria){
+    return Todo.TodoModelGet().getFromLocalStorage(criteria);
+  };
+
+  var countLeftItems = function(){
+    remaining = 0;
+    $.each( getFromLocalStorage(),function(key,value){
+      if(!value.completed && !value.deleted)
+      {
+        remaining ++;
+      }
+    });
+    return remaining;
+  };
+
+  return {
+    count : countLeftItems
+  }
+
+};
